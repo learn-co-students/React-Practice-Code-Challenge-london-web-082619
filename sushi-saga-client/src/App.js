4e0d9caad9965635c5c2022fa8684118d4aa3917
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-02 14:51:49 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,13 +7,64 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    displayIndex: 0,
+    eaten: [],
+    funds: 100
+  }
+
+  getSushi = () => {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(sushis => this.setState({ sushis }))
+  }
+
+  chooseFourSushis = () => {
+    return this.state.sushis.slice(this.state.displayIndex, this.state.displayIndex + 4)
+  }
+
+  eat = (sushiID) => {
+    const eaten = this.state.sushis.find(sushi => sushi.id === sushiID)
+    const newFunds = this.state.funds - eaten.price
+
+    this.state.funds > 0 && !this.state.eaten.includes(sushiID) && this.state.funds > eaten.price ?
+      this.setState({ funds: newFunds, eaten: [...this.state.eaten, sushiID] }) : null
+
+  }
+
+  more = (event) => {
+    let newDisplayIndex = this.state.displayIndex + 4
+
+    //bonus
+    if (newDisplayIndex >= this.state.sushis.length) {
+      newDisplayIndex = 0
+    }
+
+    this.setState({
+      displayIndex: newDisplayIndex
+    })
+  }
+
+
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushi={this.chooseFourSushis()}
+          more={this.more}
+          eat={this.eat}
+          eaten={this.state.eaten}
+
+        />
+        <Table funds={this.state.funds} eaten={this.state.eaten} />
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.getSushi()
   }
 }
 
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushi.map(sushi => <Sushi sushi={sushi} key={sushi.id} eat={props.eat} eaten={props.eaten} />)
         }
-        <MoreButton />
+        <MoreButton more={props.more} />
       </div>
     </Fragment>
   )
